c170d1614c864ea72ccaafc09904623490d58f25
Cesc Greoles <antoniogreoles@Mac-Cesc-Greoles.local>
2022-11-11 16:19:48 +0100

    First commit

---
+++ src/pages/Albumes.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Albumes = () => {
+  return <div>albumes</div>;
+};
+
+export default Albumes;
---
+++ src/pages/Artistas.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Artistas = () => {
+  return <div>artistas</div>;
+};
+
+export default Artistas;
